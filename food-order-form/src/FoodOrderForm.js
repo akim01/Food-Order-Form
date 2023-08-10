e187e27cde218137941872d9fee0a84812b9c1a9
Aki Kim <akim10.96@gmail.com>
2023-08-10 13:56:14 -0400

    commit food order form

---
+++ food-order-form/src/FoodOrderForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import styles from './FoodOrderForm.module.css';
+
+function FoodOrderForm() {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [order, setOrder] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Order Successful! Your order was ${order}. Please show your confirmation number for pickup.`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label htmlFor="name">Name: </label>
+      <input id="name" name="name" type='text' value={name} onChange={(e) => setName(e.target.value)}/>
+      <label htmlFor="phone">Phone: </label>
+      <input id="phone" name="phone" type='number' value={phone} onChange={(e) => setPhone(e.target.value)}/>
+      <label htmlFor="address">Address: </label>
+      <input id="address" name="address" type='text' value={address} onChange={(e) => setAddress(e.target.value)}/>
+      <label htmlFor="order">Order: </label>
+      <input id="order" name="order" type='text' value={order} onChange={(e) => setOrder(e.target.value)}/>
+      <button type="submit">Submit Order</button>
+    </form>
+  );
+};
+
+export default FoodOrderForm;
